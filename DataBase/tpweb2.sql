bd5f7acc807b9c7d6bc55db9a6b63518b2848a06
Lucas Quiroga <quiroga9409@gmail.com>
2022-10-10 16:53:32 -0300

    Actualizo base de datos

--- DataBase/tpweb2.sql
+++ DataBase/tpweb2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-10-2022 a las 21:07:37
+-- Tiempo de generación: 10-10-2022 a las 21:52:41
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -31,9 +31,17 @@ CREATE TABLE `account` (
   `id_account` int(11) NOT NULL,
   `id_client` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
-  `type` text NOT NULL,
-  `coin` text NOT NULL
+  `type_account` varchar(20) NOT NULL,
+  `coin` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `account`
+--
+
+INSERT INTO `account` (`id_account`, `id_client`, `amount`, `type_account`, `coin`) VALUES
+(6, 1, 2147483647, 'Caja de ahorro', 'Dolar Estadounidense'),
+(10, 2, 5000000, 'Cuenta corriente', 'Dolar Estadounidense');
 
 -- --------------------------------------------------------
 
@@ -54,7 +62,8 @@ CREATE TABLE `client` (
 
 INSERT INTO `client` (`id_client`, `dni`, `alias`, `city`) VALUES
 (0, 38178273, 'Lucas', 'Tandil'),
-(1, 41258326, 'Carreto', 'Gesell');
+(1, 41258326, 'Carreto', 'Gesell'),
+(2, 42136985, 'Marcos', 'Gesell');
 
 -- --------------------------------------------------------
 
@@ -64,7 +73,7 @@ INSERT INTO `client` (`id_client`, `dni`, `alias`, `city`) VALUES
 
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
-  `id_cliente` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `pass` varchar(250) NOT NULL,
   `email` text NOT NULL,
@@ -75,9 +84,10 @@ CREATE TABLE `user` (
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`id_user`, `id_cliente`, `username`, `pass`, `email`, `rol`) VALUES
+INSERT INTO `user` (`id_user`, `id_client`, `username`, `pass`, `email`, `rol`) VALUES
 (8, 0, 'Lucas', '$2y$10$a4pDdQtUFe2p4Yupj1PcfeMmuwABr72AwUCXZQQAG2ubJLfTMRD8e', 'quiroga9409@gmail.com', 'admin'),
-(9, 1, 'Carretto', '$2y$10$A0usUMUTK.fcmOP.0FT/ZOnVeb0BKlE8i3ErqOPeFzSAZ1xMw1eH.', 'carretto_leonardo@gmail.com', 'normal');
+(9, 1, 'Carretto', '$2y$10$A0usUMUTK.fcmOP.0FT/ZOnVeb0BKlE8i3ErqOPeFzSAZ1xMw1eH.', 'carretto_leonardo@gmail.com', 'normal'),
+(10, 2, 'Marcos', '$2y$10$8c9bb93nvbCT3pSUTG09dujlopp669ZrAM/u./4fDAnj/0CAT73gS', 'marcos_fumador@gmail.com', 'normal');
 
 --
 -- Índices para tablas volcadas
@@ -88,7 +98,7 @@ INSERT INTO `user` (`id_user`, `id_cliente`, `username`, `pass`, `email`, `rol`)
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`id_account`),
-  ADD UNIQUE KEY `FK_id_client` (`id_client`);
+  ADD KEY `FK_id_client` (`id_client`) USING BTREE;
 
 --
 -- Indices de la tabla `client`
@@ -101,17 +111,23 @@ ALTER TABLE `client`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`),
-  ADD UNIQUE KEY `id_cliente` (`id_cliente`);
+  ADD UNIQUE KEY `id_cliente` (`id_client`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `account`
+--
+ALTER TABLE `account`
+  MODIFY `id_account` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
@@ -127,7 +143,7 @@ ALTER TABLE `account`
 -- Filtros para la tabla `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `fk_client` FOREIGN KEY (`id_cliente`) REFERENCES `client` (`id_client`);
+  ADD CONSTRAINT `fk_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
